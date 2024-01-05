5713932d573da09fa0375cae83449c3e360c3b58
Steve Bumbaugh <steve@stevebumbaugh.com>
2024-01-04 23:40:01 -0500

    work in progress, form is requiring key presses before allowing submission

--- ChardSnyder/DependentCareFormFiller.js
+++ ChardSnyder/DependentCareFormFiller.js
@@ -9,6 +9,12 @@
 // ==/UserScript==
 
 
+// document.addEventListener('keydown', event => {
+//     console.log(`User pressed: ${event.key}`);
+//     event.preventDefault();
+//     return false;
+// });
+
 const fillFormButton = document.createElement('button');
 fillFormButton.textContent = 'Fill In Form';
 fillFormButton.setAttribute("style", "font-size:24px;padding: 15px;font-weight: 1000;background-color: #16F;color: white;");
@@ -19,19 +25,39 @@ buttonDiv.prepend(fillFormButton);
 
 fillFormButton.addEventListener('click', (event) => {
   let startDate = document.getElementById('dtServiceStartDate_textBox');
-  startDate.value = getMonday();
+  let monday = getMonday();
+  startDate.value = monday;
+  startDate.setAttribute("value", "monday");
+  //startDate.setAttribute("data-keeper-edited", "yes");
 
   let endDate = document.getElementById('dtServiceEndDate_textBox');
-  endDate.value = getFriday();
+  let friday = getFriday();
+  endDate.value =friday;
+  endDate.setAttribute("value", friday);
+  //endDate.setAttribute("data-keeper-edited", "yes");
 
   let amount = document.getElementById('txtAmount_textBox');
   amount.value = "210.00";
+  amount.setAttribute("value", "210.00");
+//   amount.addEventListener('keydown', event => {
+//     console.log(`User pressed: ${event.key}`);
+//     console.log('hi');
+//     event.preventDefault();
+//     return false;
+//   });
+  //amount.dispatchEvent(new KeyboardEvent('keydown', {'key': '8'}));
+  //amount.dispatchEvent(new KeyboardEvent('keyup', {'key': '8'}));
+  //amount.setAttribute("data-keeper-edited", "yes");
 
   let providerName = document.getElementById('txtProvider_textBox');
   providerName.value = "Something Special Learning Center";
+  providerName.setAttribute("value", "Something Special Learning Center");
+  //providerName.setAttribute("data-keeper-edited", "yes");
 
   let providerTaxId = document.getElementById('txtProviderSsnTaxId_textBox');
   providerTaxId.value = "34-1513894";
+  providerTaxId.setAttribute("value", "34-1513894");
+  //providerTaxId.setAttribute("data-keeper-edited", "yes");
 
   //let typeSpan = document.getElementById("ddlType");
   //let typeSelect = typeSpan.querySelector("select");
@@ -42,6 +68,8 @@ fillFormButton.addEventListener('click', (event) => {
   let dependentCheckboxes = document.getElementsByName("selectedRecipients");
   for (var i = 0; i < dependentCheckboxes.length; i++) {
     dependentCheckboxes[i].checked = true;
+    dependentCheckboxes[i].setAttribute("checked", "checked");
+    //dependentCheckboxes[i].setAttribute("data-keeper-edited", "yes");
   }
 
   let inputs = document.getElementsByClassName('LH1TextBoxHelpText');
@@ -67,5 +95,3 @@ function getFriday() {
   let monday = new Date(currentDate.setDate(diff));
   return monday.toLocaleDateString();
 }
-
-
